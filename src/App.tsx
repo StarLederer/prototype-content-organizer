234a1375a55d11daca3d0f3c28607226f2b86045
Star Lederer <germans.lederers@gmail.com>
2023-05-05 23:45:04 +0200

    feat: put a demo slider on the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import type { Component } from 'solid-js'
+import { createSignal } from 'solid-js'
+import './App.scss'
+import styles from './App.module.scss'
+import SliderInput from '~/components/SliderInput'
 
 const Main: Component = () => {
-  return undefined
+  const [val, setVal] = createSignal<number>(0)
+
+  return (
+    <form class={styles.root}>
+      <SliderInput value={val} onChange={setVal}/>
+    </form>
+  )
 }
 
 export default Main
