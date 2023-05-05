072bbf9b4a284468c113f35d58277b1a094fb4da
Star Lederer <germans.lederers@gmail.com>
2023-05-05 20:26:13 +0200

    chore: setup ESLint, Typescript and a Vite project with SolidJS

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+
+  plugins: [solidPlugin()],
+})
