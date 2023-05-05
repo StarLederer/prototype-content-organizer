00ce6e863781a028768a97a4cd6d01852c958f11
Star Lederer <germans.lederers@gmail.com>
2023-05-05 23:43:55 +0200

    feat: add styles to the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { render } from 'solid-js/web'
 import App from './App'
+import './style'
+import { cssProps } from './style/constants'
 
-const root = document.getElementById('root')
+const root = document.getElementById('root')!
 
-render(() => <App />, root!)
+cssProps().forEach(([key, val]) => {
+  root.style.setProperty(key, val)
+})
+
+render(() => <App />, root)
