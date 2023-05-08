4295ce424d328e4616ac5bce530bb7479cf98c5e
Star Lederer <germans.lederers@gmail.com>
2023-05-08 21:48:41 +0200

    feat: remove unnecessary wrapper dive from `App`

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import type { Component } from 'solid-js'
 import { For } from 'solid-js'
 import './App.scss'
-import styles from './App.module.scss'
 import Entry from '~/components/Entry'
 import ContextMenu from '~/components/contextMenu/Provider'
 
 const Main: Component = () => {
   return (
     <ContextMenu>
-      <div class={styles.root}>
-        <For each={Array(4)}>
-          {() => <Entry />}
-        </For>
-      </div>
+      <For each={Array(4)}>
+        {() => <Entry />}
+      </For>
     </ContextMenu>
   )
 }
