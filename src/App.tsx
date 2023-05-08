dff78d9bb5d1af74dbc5eb61a19679a47547ba72
Star Lederer <germans.lederers@gmail.com>
2023-05-08 21:45:38 +0200

    feat: add custom context menu to `App`

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { For } from 'solid-js'
 import './App.scss'
 import styles from './App.module.scss'
 import Entry from '~/components/Entry'
+import ContextMenu from '~/components/contextMenu/Provider'
 
 const Main: Component = () => {
   return (
-    <div class={styles.root}>
-      <For each={Array(4)}>
-        {() => <Entry />}
-      </For>
-    </div>
+    <ContextMenu>
+      <div class={styles.root}>
+        <For each={Array(4)}>
+          {() => <Entry />}
+        </For>
+      </div>
+    </ContextMenu>
   )
 }
 
