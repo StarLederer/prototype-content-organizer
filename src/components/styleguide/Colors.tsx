ce775e48d6ba024bdffa86b12705df89172828d3
Star Lederer <germans.lederers@gmail.com>
2023-05-28 14:25:25 +0200

    feat: add 'panel' colors

--- src/components/styleguide/Colors.tsx
+++ src/components/styleguide/Colors.tsx
@@ -18,6 +18,18 @@ const ColorCards: Component = () => (
 
     <For each={Array(4)}>
       {(_, acc) => (
+        <div data-color data-panel={acc() + 1}>
+          <For each={Array(6)}>
+            {(_, fg) => (
+              <div>{fg() + 1}</div>
+            )}
+          </For>
+        </div>
+      )}
+    </For>
+
+    <For each={Array(4)}>
+      {(_, acc) => (
         <div data-color data-accent={acc() + 1}>
           <For each={Array(6)}>
             {(_, fg) => (
