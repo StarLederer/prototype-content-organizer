efb676fb4f0f04bb7093efe376ad78f22ffc2983
Star Lederer <germans.lederers@gmail.com>
2023-05-28 14:28:08 +0200

    feat: update transitions
    `-ease-in-out` and `--ease-in-out-overshoot` are no longer available

--- src/style/constants.ts
+++ src/style/constants.ts
@@ -4,10 +4,10 @@ export const duration4 = duration2 * 2
 export const duration8 = duration4 * 2
 export const duration16 = duration8 * 2
 export const duration32 = duration16 * 2
-export const ease: [number, number, number, number] = [0.25, 0.75, 0, 1]
-export const easeOvershoot: [number, number, number, number] = [0.25, 0.75, 0, 1.125]
-export const easeInOut: [number, number, number, number] = [0.25, 0.125, 0, 1]
-export const easeInOutOvershoot: [number, number, number, number] = [0.25, 0.75, 0, 1.125]
+export const ease: [number, number, number, number] = [0.25, 0.5, 0, 1]
+export const easeOvershoot: [number, number, number, number] = [0.25, 0.5, 0, 1.5]
+export const easeOut: [number, number, number, number] = [0, 1, 0.5, 1]
+export const easeOutOvershoot: [number, number, number, number] = [0.25, 1.5, 0.5, 1]
 
 export function cssProps(): [string, string][] {
   return [
@@ -19,7 +19,7 @@ export function cssProps(): [string, string][] {
     ['--duration-x32', `${duration32}s`],
     ['--ease', `cubic-bezier(${ease.join(',')})`],
     ['--ease-overshoot', `cubic-bezier(${easeOvershoot.join(',')})`],
-    ['--ease-in-out', `cubic-bezier(${easeInOut.join(',')})`],
-    ['--ease-in-out-overshoot', `cubic-bezier(${easeInOutOvershoot.join(',')})`],
+    ['--ease-out', `cubic-bezier(${easeOut.join(',')})`],
+    ['--ease-out-overshoot', `cubic-bezier(${easeOutOvershoot.join(',')})`],
   ]
 }
