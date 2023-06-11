44344fa71682de14ae8684c349dd264c0f72c4c7
Star Lederer <germans.lederers@gmail.com>
2023-06-11 13:44:45 +0200

    docs: add `PlaybackBar` stories

---
+++ src/components/atoms/PlaybackBar.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from 'storybook-solidjs'
+import Component from './PlaybackBar'
+
+const meta = {
+  component: Component,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Component>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Main: Story = {}
