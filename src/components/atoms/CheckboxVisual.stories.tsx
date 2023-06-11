5f1a799c0fc1f19d75b9fcf0ede544ce66131cf9
Star Lederer <germans.lederers@gmail.com>
2023-06-11 12:27:09 +0200

    docs: add `CheckboxVisual` stories

---
+++ src/components/atoms/CheckboxVisual.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from 'storybook-solidjs'
+import Component from './CheckboxVisual'
+
+const meta = {
+  component: Component,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Component>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Idle: Story = {}
+
+export const Active: Story = {
+  args: {
+    active: true,
+  },
+}
